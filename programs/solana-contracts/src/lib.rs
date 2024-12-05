10b24d5aa525f70b0047694151a90ffaee833a7d
andrew <superfulanki@126.com>
2024-12-05 15:41:23 +0800

    增加铸币方法

--- programs/solana-contracts/src/lib.rs
+++ programs/solana-contracts/src/lib.rs
@@ -1,9 +1,11 @@
 #![allow(clippy::result_large_err)]
 use anchor_lang::prelude::*;
-
+use std::collections::LinkedList;
 pub mod tokens;
+pub mod nft;
 
 use tokens::*;
+use nft::*;
 
 declare_id!("FQcgiPK79ZFAtBriM2MjVGeDYVHu9zDdSwmGqD1CaEoS");
 
@@ -21,11 +23,22 @@ pub mod solana_contracts {
     }
 
     pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
-        mint::mint_token(ctx, amount)
+        tokens::mint::mint_token(ctx, amount)
     }
 
     pub fn transfer_tokens(ctx: Context<TransferTokens>, amount: u64) -> Result<()> {
-        transfer::transfer_tokens(ctx, amount)
+        tokens::transfer::transfer_tokens(ctx, amount)
+    }
+
+    pub fn create_nft(
+        ctx: Context<NFTToken>,
+        token_title: String,
+        token_symbol: String,
+        token_uri: String,
+        supply_no: u64,
+        assets: LinkedList<Asset>
+    ) -> Result<()> {
+        nft::mint::mint(ctx, token_title, token_symbol, token_uri, supply_no, assets)
     }
 }
 
