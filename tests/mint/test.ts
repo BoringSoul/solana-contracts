0f101cf273fc768d127e6150cc5de266b1a07d67
andrew <superfulanki@126.com>
2024-12-07 16:54:47 +0800

    修改参数

--- tests/mint/test.ts
+++ tests/mint/test.ts
@@ -2,6 +2,7 @@ import * as anchor from '@coral-xyz/anchor';
 import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 import { Keypair } from '@solana/web3.js';
 import type { SolanaContracts } from '../../target/types/solana_contracts';
+import { BN } from 'bn.js';
 
 describe('NFT Minter', () => {
   const provider = anchor.AnchorProvider.env();
@@ -14,8 +15,8 @@ describe('NFT Minter', () => {
     name: 'Homer NFT',
     symbol: 'HOMR',
     uri: 'https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/nft.json',
-    supply_no: Number(1),
-    assets: [{"amount": Number(100000), "token_address": 'thisisthattokenaddress'}]
+    supply_no: new BN(1),
+    assets: [{"amount": new BN(10000), "token_address": 'thisisthattokenaddress'}]
   };
 
   it('Create an NFT!', async () => {
