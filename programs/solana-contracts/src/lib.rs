ec903a57e5422e6f8c18cd887a9a93767b7bab1e
andrew <superfulanki@126.com>
2024-12-21 03:01:33 +0800

    新增版本

--- programs/solana-contracts/src/lib.rs
+++ programs/solana-contracts/src/lib.rs
@@ -4,9 +4,9 @@ pub mod asset;
 pub mod tokens;
 pub mod nft;
 pub mod errors;
-use tokens::*;
+// use tokens::*;
 use nft::*;
-use asset::*;
+// use asset::*;
 
 // declare_id!("FQcgiPK79ZFAtBriM2MjVGeDYVHu9zDdSwmGqD1CaEoS");
 // declare_id!("DvYFdnuamvwsxXDaHYAzfLSZ8AbDQd9BY2x71j6s87cs");
@@ -16,38 +16,45 @@ pub mod solana_contracts {
 
     use super::*;
 
-    pub fn create_token(
-        ctx: Context<CreateToken>,
-        token_title: String,
-        token_symbol: String,
-        token_uri: String,
-    ) -> Result<()> {
-        create::create_token(ctx, token_title, token_symbol, token_uri)
-    }
+    // pub fn create_token(
+    //     ctx: Context<CreateToken>,
+    //     token_title: String,
+    //     token_symbol: String,
+    //     token_uri: String,
+    // ) -> Result<()> {
+    //     create::create_token(ctx, token_title, token_symbol, token_uri)
+    // }
 
-    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
-        tokens::mint::mint_token(ctx, amount)
-    }
+    // pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> Result<()> {
+    //     tokens::mint::mint_token(ctx, amount)
+    // }
 
-    pub fn transfer_tokens(ctx: Context<TransferTokens>, amount: u64) -> Result<()> {
-        tokens::transfer::transfer_tokens(ctx, amount)
-    }
+    // pub fn transfer_tokens(ctx: Context<TransferTokens>, amount: u64) -> Result<()> {
+    //     tokens::transfer::transfer_tokens(ctx, amount)
+    // }
 
     pub fn mint_nft(
         ctx: Context<MintNft>,
         token_title: String,
         token_symbol: String,
         token_uri: String,
-        assets_address: String
-    ) -> Result<()> {
-        nft::mint_nft::mint_nft(ctx, token_title, token_symbol, token_uri, assets_address)
-    }
-
-    pub fn wrap_asset(
-        ctx:Context<WrapContext>,
         supply_no:u64,
         assets: Vec<asset::Asset>
-    ) -> Result<()>{
-        asset::wrap(ctx, supply_no, assets)
+    ) -> Result<()> {
+        ctx.accounts.mint(token_title, token_symbol, token_uri, supply_no, assets)
     }
+
+    pub fn burn_nft(
+        ctx: Context<BurnNft>,
+    ) -> Result<()> {
+        ctx.accounts.burn_nft()
+    }
+
+    // pub fn wrap_asset(
+    //     ctx:Context<WrapContext>,
+    //     supply_no:u64,
+    //     assets: Vec<asset::Asset>
+    // ) -> Result<()>{
+    //     asset::wrap(ctx, supply_no, assets)
+    // }
 }
--- programs/solana-contracts/src/nft/burn_nft.rs
+++ programs/solana-contracts/src/nft/burn_nft.rs
@@ -1,42 +1,38 @@
-use anchor_lang::prelude::*;
-use anchor_spl::{
-    token_2022,
-    token_interface::Token2022,
+#![allow(clippy::result_large_err)]
+
+use {
+    anchor_lang::prelude::*, anchor_spl::{
+        metadata::{mpl_token_metadata::types::DataV2, Metadata}, token::{ burn, Burn, Token, TokenAccount}
+    }
 };
-
-
-// 新增 BurnNft 指令
-pub fn burn_nft(ctx: Context<BurnNft>) -> Result<()> {
-    let seeds = b"nft_authority";
-    let bump = ctx.bumps.nft_authority;
-    let signer: &[&[&[u8]]] = &[&[seeds, &[bump]]];
-
-    token_2022::burn(
-        CpiContext::new_with_signer(
-            ctx.accounts.token_program.to_account_info(),
-            token_2022::Burn {
-                mint: ctx.accounts.mint.to_account_info(),
-                from: ctx.accounts.token_account.to_account_info(),
-                authority: ctx.accounts.nft_authority.to_account_info(),
-            },
-            signer
-        ),
-        1
-    )?;
-
-    Ok(())
-}
-
 #[derive(Accounts)]
 pub struct BurnNft<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
     #[account(mut)]
-    pub token_account: AccountInfo<'info>,
+    pub token_account: Account<'info, TokenAccount>,
     #[account(mut)]
-    pub mint: AccountInfo<'info>,
-    #[account(init_if_needed, seeds = [b"nft_authority".as_ref()], bump, space = 8, payer = signer)]
-    pub nft_authority: Account<'info, crate::mint_nft::NftAuthority>,
+    pub mint_account: AccountInfo<'info>,
+    // #[account(init_if_needed, seeds = [b"nft_authority".as_ref()], bump, space = 8, payer = signer)]
+    // pub nft_authority: Account<'info, crate::mint_nft::NftAuthority>,
+    pub token_metadata_program: Program<'info, Metadata>,
     pub system_program: Program<'info, System>,
-    pub token_program: Program<'info, Token2022>,
+    pub token_program: Program<'info, Token>,
 }
+
+impl<'info> BurnNft<'info> {
+    pub fn burn_nft(&mut self) -> Result<()> {
+        let metadata = self.token_metadata_program.deserialize_data::<AccountInfo<'info>>();
+        burn(CpiContext::new(self.token_program.to_account_info(), 
+        Burn {
+                mint:self.mint_account.to_account_info(),
+                from:self.token_account.to_account_info(),
+                authority:self.signer.to_account_info(),
+            }), 
+            1)
+    }
+}
+
+// 新增 BurnNft 指令
+
+
