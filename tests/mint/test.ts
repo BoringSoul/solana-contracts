cd2acae902637c279fded7a1c8c8d3f41770ddf7
andrew <superfulanki@126.com>
2024-12-22 11:49:55 +0800

    修正引用方法

--- tests/mint/test.ts
+++ tests/mint/test.ts
@@ -25,7 +25,7 @@ describe('NFT Minter', () => {
     // Derive the associated token address account for the mint and payer.
     const associatedTokenAccountAddress = getAssociatedTokenAddressSync(mintKeypair.publicKey, payer.publicKey);
     const transactionSignature = await program.methods
-      .createNft(metadata.name, metadata.symbol, metadata.uri, metadata.supply_no, metadata.assets)
+      .mintNft(metadata.name, metadata.symbol, metadata.uri, metadata.supply_no, metadata.assets)
       // .createNft(metadata.name, metadata.symbol, metadata.uri, metadata.assets)
       // .createNft(metadata.name, metadata.symbol, metadata.uri, metadata.supply_no)
       // .createNft(metadata.name, metadata.symbol, metadata.uri)
