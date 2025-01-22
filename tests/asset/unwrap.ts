fc2111f377ee901b90a63ca2141946eb9c37bcfa
andrew <superfulanki@126.com>
2025-01-22 23:59:33 +0800

    修改unwrap测试方法

--- tests/asset/unwrap.ts
+++ tests/asset/unwrap.ts
@@ -6,7 +6,7 @@ import type { SolanaContracts } from '../target/types/solana_contracts';
 import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 import { min } from 'bn.js';
 
-describe('Stake And Unstake Test', () => {
+describe('资产测试-解包', () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -17,29 +17,21 @@ describe('Stake And Unstake Test', () => {
   const payer = Keypair.fromSecretKey(new Uint8Array(payerSeed));
   console.log(`payer: ${payer.publicKey}`);
 
-  // const mintKeypair = Keypair.fromSecretKey(new Uint8Array([6,228,116,176,184,251,15,87,10,226,14,84,30,119,2,141,147,191,50,161,150,176,158,131,98,77,157,108,87,169,250,207,38,110,92,181,25,227,103,240,156,104,148,209,32,181,189,58,248,161,239,37,232,156,124,158,130,104,211,183,120,123,105,69]));
- 
 
-  const authoritySeed  = [124,247,111,86,69,22,243,227,110,29,54,161,239,132,170,253,72,105,113,100,66,59,213,229,22,66,62,68,5,241,98,168,164,129,238,19,203,65,76,173,153,230,208,0,254,62,123,163,8,44,142,208,150,74,245,209,159,211,123,137,100,76,84,97];
-  const authority = Keypair.fromSecretKey(new Uint8Array(authoritySeed));
-  console.log(`authority: ${authority.publicKey}`);
-
-  const [assetManagerAddress] = PublicKey.findProgramAddressSync([Buffer.from('asset_manager'), authority.publicKey.toBuffer()], program.programId);
-  console.log(`assetManagerAddress: ${assetManagerAddress}`);
+  const assetCollection = new PublicKey("9ZRVL57MtSDk26k6oSy1Zbm1n1XXXFm7dePSgqeNr6Kq");
+  console.log(`assetCollection: ${assetCollection}`);
   const supplyNo = new anchor.BN(1);
   //MjadGGZfdowLjJbhf2xXUmGuydYtRV2AtCzbfzA9RCs
-  const [assetAddress ] = PublicKey.findProgramAddressSync([Buffer.from('asset'), assetManagerAddress.toBuffer(), supplyNo.toBuffer("le", 8)], program.programId);
+  const [assetAddress] = PublicKey.findProgramAddressSync([Buffer.from('asset'), assetCollection.toBuffer(), supplyNo.toBuffer("le", 8)], program.programId);
   console.log(`assetAddress: ${assetAddress}`);
-  it('解包', async () => {
+  it('开始解包', async () => {
     await program.methods
     .unwrapAsset(supplyNo)
     .accounts({
       owner: payer.publicKey,
-      authority: authority.publicKey,
-      assetManager: assetManagerAddress,
       asset:assetAddress
     })
-    .signers([payer, authority])
+    .signers([payer])
     .rpc();
   });
 
