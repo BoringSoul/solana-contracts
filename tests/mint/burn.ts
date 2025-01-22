c9922f8613584c6acd2f79e81d9bbfc9d25b16b3
andrew <superfulanki@126.com>
2025-01-23 00:17:26 +0800

    修改burn测试脚本

--- tests/mint/burn.ts
+++ tests/mint/burn.ts
@@ -5,7 +5,7 @@ import {Keypair ,PublicKey } from '@solana/web3.js';
 import type { SolanaContracts } from '../target/types/solana_contracts';
 import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 
-describe('wrap asset', () => {
+describe('铸币销毁', () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -15,30 +15,30 @@ describe('wrap asset', () => {
   const payerSeed = [137,208,207,116,77,151,229,89,133,115,90,11,15,150,170,146,200,189,204,252,244,79,250,34,105,231,32,148,110,252,2,170,102,19,226,76,136,8,58,32,103,157,208,48,146,135,1,168,101,56,43,148,65,86,17,34,219,137,210,117,58,219,104,180];
   const payer = Keypair.fromSecretKey(new Uint8Array(payerSeed));
 
-  const authoritySeed  = [124,247,111,86,69,22,243,227,110,29,54,161,239,132,170,253,72,105,113,100,66,59,213,229,22,66,62,68,5,241,98,168,164,129,238,19,203,65,76,173,153,230,208,0,254,62,123,163,8,44,142,208,150,74,245,209,159,211,123,137,100,76,84,97];
-  const authority = Keypair.fromSecretKey(new Uint8Array(authoritySeed));
-
-  const supplyNo = new anchor.BN(1);
-  const [assetInfoAccountAddr] = PublicKey.findProgramAddressSync([Buffer.from('asset_manager'), authority.publicKey.toBuffer()], program.programId);
-  const [assetAddress ] = PublicKey.findProgramAddressSync([Buffer.from('asset'), assetInfoAccountAddr.toBuffer(), supplyNo.toBuffer("le", 8)], program.programId);
-  
+  const supplyNo = new anchor.BN(3);
+ 
+  const assetCollection = new PublicKey("B4Qn9uFmvdHb8YqEcbo7Dk41jvsSeiLDSBXjS4xuQDp9");
+  console.log(`assetCollection: ${assetCollection}`);
+  //MjadGGZfdowLjJbhf2xXUmGuydYtRV2AtCzbfzA9RCs
+  const [assetAddress] = PublicKey.findProgramAddressSync([Buffer.from('asset'), assetCollection.toBuffer(), supplyNo.toBuffer("le", 8)], program.programId);
+  console.log(`assetAddress: ${assetAddress}`);
 
   console.log(`payer: ${payer.publicKey}`);
-  console.log(`authority: ${authority.publicKey}`);
-  console.log(`programID = ${program.programId}`)
-  console.log(`  assetInfoAccountAddr : ${assetInfoAccountAddr}`);
+  console.log(`  assetAddress : ${assetAddress}`);
 
-  it('burn', async () => {
+  console.log(`payer: ${payer.publicKey}`);
+  console.log(`programID = ${program.programId}`)
+  console.log(`  assetAddress : ${assetAddress}`);
+
+  it('开始销毁', async () => {
     // Generate a keypair to use as the address of our mint account
     const transactionSignature = await program.methods
       .burnNft(supplyNo)
       .accounts({
         owner: payer.publicKey,
-        authority: authority.publicKey,
-        assetManager: assetInfoAccountAddr,
         asset: assetAddress,
       })
-    .signers([payer, authority])
+    .signers([payer])
     .rpc({ skipPreflight: true });
     console.log('Success!');
     console.log(` Burn Transaction Signature: ${transactionSignature}`);
