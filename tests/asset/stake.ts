f69d0e97dc8d5d1b968cac164a538a7c2002b3e0
andrew <superfulanki@126.com>
2025-01-23 00:28:49 +0800

    修改stake测试脚本

--- tests/asset/stake.ts
+++ tests/asset/stake.ts
@@ -6,7 +6,7 @@ import type { SolanaContracts } from '../target/types/solana_contracts';
 import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 import { min } from 'bn.js';
 
-describe('Stake And Unstake Test', () => {
+describe('质押测试', () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -17,30 +17,28 @@ describe('Stake And Unstake Test', () => {
   const payer = Keypair.fromSecretKey(new Uint8Array(payerSeed));
   console.log(`payer: ${payer.publicKey}`);
 
-  // const mintKeypair = Keypair.fromSecretKey(new Uint8Array([6,228,116,176,184,251,15,87,10,226,14,84,30,119,2,141,147,191,50,161,150,176,158,131,98,77,157,108,87,169,250,207,38,110,92,181,25,227,103,240,156,104,148,209,32,181,189,58,248,161,239,37,232,156,124,158,130,104,211,183,120,123,105,69]));
- 
 
   const authoritySeed  = [124,247,111,86,69,22,243,227,110,29,54,161,239,132,170,253,72,105,113,100,66,59,213,229,22,66,62,68,5,241,98,168,164,129,238,19,203,65,76,173,153,230,208,0,254,62,123,163,8,44,142,208,150,74,245,209,159,211,123,137,100,76,84,97];
   const authority = Keypair.fromSecretKey(new Uint8Array(authoritySeed));
   console.log(`authority: ${authority.publicKey}`);
 
-  const [assetManagerAddress] = PublicKey.findProgramAddressSync([Buffer.from('asset_manager'), authority.publicKey.toBuffer()], program.programId);
-  console.log(`assetManagerAddress: ${assetManagerAddress}`);
-  const supplyNo = new anchor.BN(1);
+  const supplyNo = new anchor.BN(4);
+  const assetCollection = new PublicKey("B4Qn9uFmvdHb8YqEcbo7Dk41jvsSeiLDSBXjS4xuQDp9");
+  console.log(`assetCollection: ${assetCollection}`);
   //MjadGGZfdowLjJbhf2xXUmGuydYtRV2AtCzbfzA9RCs
-  const [assetAddress ] = PublicKey.findProgramAddressSync([Buffer.from('asset'), assetManagerAddress.toBuffer(), supplyNo.toBuffer("le", 8)], program.programId);
+  const [assetAddress] = PublicKey.findProgramAddressSync([Buffer.from('asset'), assetCollection.toBuffer(), supplyNo.toBuffer("le", 8)], program.programId);
   console.log(`assetAddress: ${assetAddress}`);
-  it('质押', async () => {
+
+  it('开始质押', async () => {
     const stakeNo = new anchor.BN(1);
     await program.methods
     .stake(stakeNo)
     .accounts({
       owner: payer.publicKey,
       authority: authority.publicKey,
-      assetManager: assetManagerAddress,
       asset:assetAddress
     })
-    .signers([payer, authority])
+    .signers([payer])
     .rpc();
   });
 
